202d99c05da3af45e49867f7aabc8af46f39e443
ac906 <ajec203@exeter.ac.uk>
2026-03-04 13:10:44 +0000

    adding updates to final 3 functions from me and toby

--- cityrescue-starter/src/main/java/cityrescue/CityRescueImpl.java
+++ cityrescue-starter/src/main/java/cityrescue/CityRescueImpl.java
@@ -2,6 +2,8 @@ package cityrescue;
 
 import java.nio.channels.UnsupportedAddressTypeException;
 
+
+
 import cityrescue.enums.*;
 import cityrescue.exceptions.*;
 
@@ -49,20 +51,21 @@ public class CityRescueImpl implements CityRescue {
         // set the tick = 0 
         tick = 0;
         
-        //set counters = 0
+        //set counters for all = 0
         stationCounter = 0;
         unitCounter = 0;
         incidentCounter = 0;
 
-        // Recreate empty arrays
+        // create the arrays with the right amount of space 
         stations = new Station[MAX_STATIONS];
         units = new Unit[MAX_UNITS];
         incidents = new Incident[MAX_INCIDENTS];
 
-        //set the next ids = 1 
-        nextIncidentId =1;
+        //reset the nextIDs
+        nextIncidentId = 1;
         nextStationId =1;
         nextUnitId =1;
+
     }
 
     @Override
@@ -95,22 +98,23 @@ public class CityRescueImpl implements CityRescue {
     cityMap.setBlocked(x, y, false); 
     }
 
+        
     @Override
     public int addStation(String name, int x, int y) throws InvalidNameException, InvalidLocationException {
         if (! cityMap.validCoords(x, y) || cityMap.isBlocked(x, y)){
         throw new InvalidLocationException("Tile is out of range or blocked");
-    }   if (name == null) {  // covers the case when it's blank  
+    }   if (name == null) {  // covers the case when nothing inputted 
         throw new InvalidNameException("Enter a valid name");     
     }   
-        stations[stationCounter]= new Station(StationId, name, x,y);
+        stations[stationCounter]= new Station(nextStationId, name, x,y);
         stationCounter++;
-        StationId++; // returns original ID then adds 1 
+        return nextStationId++; 
     }
 
     @Override
     public void removeStation(int stationId) 
     throws IDNotRecognisedException, IllegalStateException{
-        int position = -10;
+        int position = 21;
         for (int i = 0; i < stationCounter; i++) {
             if (stations[i].getId() == stationId) {
             position = i;
@@ -118,7 +122,7 @@ public class CityRescueImpl implements CityRescue {
         }
         }
         // error if the station is not found 
-        if (position == -10){
+        if (position == 21){
             throw new IDNotRecognisedException("Station doesn't exist");
         }
         // error if the station has no units 
@@ -161,18 +165,19 @@ public class CityRescueImpl implements CityRescue {
                 throw new InvalidCapacityException("input for maximum units is invalid");
             }
             station.setCapacity(maxUnits);
-        }
+            }
+
+        
 
 
     @Override
     public int[] getStationIds() {
         // need an array of the size of station count 
         int[] stationIds = new int[stationCounter];
-        for (int i=0; i < stationCounter-1; i++){
+        for (int i=0; i < stationCounter; i++){
             stationIds[i]=stations[i].getId();
         } 
-        // this list is natually in acsending order because stations added are always added 
-        // to the end of the list and removing stations just shifts left  
+        // need a way to sort the array in ascending order 
         return stationIds;
     }
 
@@ -224,6 +229,7 @@ public class CityRescueImpl implements CityRescue {
         unitCounter++;
         return nextUnitId++;
     }
+    
 
     @Override
     public void decommissionUnit(int unitId) throws IDNotRecognisedException, IllegalStateException {
@@ -242,9 +248,11 @@ public class CityRescueImpl implements CityRescue {
             units[unitPosition].getUnitStatus() == UnitStatus.AT_SCENE){
                 throw new IllegalStateException("Unit is busy");
             }
+        unitCounter--; // take 1 away from the unit count 
         for(int k= unitPosition; k< unitCounter; k++){
             units[k] = units[k + 1]; // shifts all the units beyond, down 1 place 
         }
+        units[unitCounter] = null; // set the last space to null otherwise get duplicates
     }
 
     @Override
@@ -268,9 +276,7 @@ public class CityRescueImpl implements CityRescue {
             throw new IDNotRecognisedException("Unit/Station was not found");
         }
         int unitCountAtStation = 0;
-        for(int j = 0;
-                j < stationCounter;j++){ 
-                    if(units[j].getStationID() == newStationId){
+        for(int j = 0; j < unitCounter;j++){ if(units[j].getStationID() == newStationId){
                     unitCountAtStation++;}}
 
         if(units[unitPosition].getUnitStatus() == UnitStatus.AT_SCENE || 
@@ -308,11 +314,11 @@ public class CityRescueImpl implements CityRescue {
 
     @Override
     public int[] getUnitIds() {
-        int unitIds[] = new int[unitCounter]; // set up a list with as many positions as unit count
+        int unitIds[] = new int[unitCounter];
         for (int i=0; i< unitCounter; i++){
             unitIds[i] = units[i].getUnitId();
         }
-        // naturally they are in accendung order, as a new unit is added to the end of the list
+        // need to sort them in accessending order too 
         return unitIds;
     }
 
@@ -344,7 +350,7 @@ public class CityRescueImpl implements CityRescue {
             throw new InvalidSeverityException("Severity is not in bounds");}
         // check that the location is within the bounds of the gridsize
         int[] grid = getGridSize();
-        if(grid[0] >= x || x<0 || y<0 || grid[1] >= y || cityMap.isBlocked(x, y)){
+        if(grid[0] < x || x<0 || y<0 || grid[1] < y || cityMap.isBlocked(x, y)){
             throw new InvalidLocationException("location is out of bounds");
         }
         // create the incident report, index it using the incident count
@@ -378,7 +384,6 @@ public class CityRescueImpl implements CityRescue {
                     units[k].setIncident(-10); // arbritary value that isnt +ve
                     units[k].setWorkLeft(0); // there will be no work remaining
                     incidents[incidentPosition].setStatus(IncidentStatus.CANCELLED);
-                    // runs until all units assigned to incident are cancelled
                 }
             }
         } else { throw new IllegalStateException("Can't cancel the indident");}
@@ -402,7 +407,7 @@ public class CityRescueImpl implements CityRescue {
         }
         if (incidents[incidentPosition].getIncidentStatus() == IncidentStatus.CANCELLED||
             incidents[incidentPosition].getIncidentStatus() == IncidentStatus.RESOLVED){
-            throw new InvalidSeverityException("The incident status means this is not valid");    
+            throw new IllegalStateException("The incident status means this is not valid");    
         }
         incidents[incidentPosition].setSeverity(newSeverity); // change the severity to the new one 
     }
@@ -447,19 +452,239 @@ public class CityRescueImpl implements CityRescue {
 
     @Override
     public void dispatch() {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not implemented yet");
+        // find the incidents that are reported 
+        // the incident array is already in ascending order 
+        for (int i=0; i < incidentCounter; i++){
+            if(incidents[i].getIncidentStatus() != IncidentStatus.REPORTED){
+                continue;  // skip iteration if not got reported status
+            }
+            // now need to find all the units which are eligeble 
+            Unit bestUnit = null;
+            int bestDist = 0;
+            for(int j=0; j< unitCounter; j++){
+                // can't have units that are null
+                if(units[j] == null){ continue;}
+                // ensure IDLE as needed to be eligeble
+                if(units[j].getUnitStatus() != UnitStatus.IDLE){continue;}
+                // now need to check that the incident can be handled by this unit
+                if(!units[j].incidentsResolve(incidents[i].getIncidentType())){continue;}
+                // anything that has got to here is a valid option but now 
+                // get the manhattan distance 
+                int dist = Math.abs(units[j].getX() - incidents[i].getX()) +
+                Math.abs(units[j].getY() - incidents[i].getY());
+                if(bestUnit==null){
+                    bestUnit = units[j];
+                    bestDist = dist;
+                } 
+                else if(dist < bestDist // now deal with a tie where choose lower unit ID
+                    || (dist == bestDist && units[j].getUnitId() < bestUnit.getUnitId())
+                    // now deal with lower station ID
+                    || (dist == bestDist && units[j].getUnitId() == bestUnit.getUnitId()
+                        && units[j].getStationID() < bestUnit.getStationID())) {
+
+                bestUnit = units[j];
+                bestDist = dist;
+                }
+            }
+            if(bestUnit == null){continue;} // can't do anything in this case just don't assign
+            // dispach that incident and set status to EN ROUTE
+            incidents[i].setStatus(IncidentStatus.DISPATCHED);
+            bestUnit.setUnitStatus(UnitStatus.EN_ROUTE);
+            bestUnit.setIncident(incidents[i].getIncidentId());
+        }
     }
 
     @Override
     public void tick() {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not implemented yet");
+        tick++;
+        // need to move the units EN_ROUTE
+        for(int i =0; i< unitCounter;i++){
+            // first get rid of the cases where unit doesnt need to be moved
+            if(units[i] == null){continue;}
+            boolean arrivedThisTick = false;
+           // ONLY movement is restricted to EN_ROUTE
+            if(units[i].getUnitStatus() == UnitStatus.EN_ROUTE){
+            // need to get the incident position
+            int incidentX = -10;
+            int incidentY = -10;
+            for(int j=0; j< incidentCounter; j++){
+                if(incidents[j].getIncidentId() == units[i].getIncident()){
+                    incidentX = incidents[j].getX();
+                    incidentY = incidents[j].getY();
+                    break;
+                }
+            }
+            //now need to get the distance from incident
+            int distNow = Math.abs(units[i].getX() - incidentX) + 
+            Math.abs(units[i].getY() - incidentY);
+
+            //boolean variable for if the unit has been moved
+            boolean moved = false;
+            // check that the move would be valid in terms of board and blocking
+            if (units[i].getY() > 0 && !cityMap.isBlocked(units[i].getX(),units[i].getY() -1)) {
+                // now get the distance to the incident
+                int newDist = Math.abs(units[i].getX() - incidentX) + Math.abs((units[i].getY() - 1) - incidentY);
+                if (newDist < distNow) {
+                units[i].setY(units[i].getY() -1); // since closer change the Y value
+                moved = true; // set moved to true 
+            }
+            }
+            // now basically repeat for EAST, then SOUTH, then WEST
+            if (!moved && units[i].getX() < cityMap.getWidth() -1 && !cityMap.isBlocked(units[i].getX()+1,units[i].getY())) {
+                int newDist = Math.abs(units[i].getX()+1 - incidentX) + Math.abs((units[i].getY()) - incidentY);
+                if (newDist < distNow) {
+                units[i].setX(units[i].getX() +1); 
+                moved = true; 
+            }
+            }
+            if (!moved && units[i].getY() < cityMap.getHeight() -1 && !cityMap.isBlocked(units[i].getX(),units[i].getY()+1)) {
+                int newDist = Math.abs(units[i].getX() - incidentX) + Math.abs(units[i].getY()+1 - incidentY);
+                if (newDist < distNow) {
+                units[i].setY(units[i].getY() +1); 
+                moved = true; 
+            }
+            }
+            if (!moved && units[i].getX() > 0 && !cityMap.isBlocked(units[i].getX()-1,units[i].getY())) {
+                // now get the distance to the incident
+                int newDist = Math.abs(units[i].getX()-1 - incidentX) + Math.abs(units[i].getY() - incidentY);
+                if (newDist < distNow) {
+                units[i].setX(units[i].getX() -1); // since closer change the Y value
+                moved = true; // set moved to true 
+            }
+            }
+            // now at this point if there was no legal move, we need to make a move irrespective
+            // of distance and instead just order N,E,S,W
+            if(!moved) {
+            // do north first
+            if(units[i].getY() > 0 && !cityMap.isBlocked(units[i].getX(), units[i].getY() - 1)) {
+                units[i].setY(units[i].getY() - 1);
+                moved = true;
+            }
+            // then east 
+            if(!moved && units[i].getX() < cityMap.getWidth() - 1 && !cityMap.isBlocked(units[i].getX() + 1, units[i].getY())) {
+                units[i].setX(units[i].getX() + 1);
+                moved = true;
+            }
+            // then south
+            if(!moved && units[i].getY() < cityMap.getHeight() - 1 && !cityMap.isBlocked(units[i].getX(), units[i].getY() + 1)) {
+                units[i].setY(units[i].getY() + 1);
+                moved = true;
+            }
+            // then west
+            if(!moved && units[i].getX() > 0 && !cityMap.isBlocked(units[i].getX() - 1, units[i].getY())) {
+                units[i].setX(units[i].getX() - 1);
+                moved = true;
+            }
+            }
+            // at this point if still not managed to move then 
+            // now need to mark arrival of a unit if applicable
+            if(units[i].getX() == incidentX && units[i].getY() == incidentY){
+                if(units[i].getUnitStatus() != UnitStatus.AT_SCENE){ // if not alreadu then new 
+                    units[i].setUnitStatus(UnitStatus.AT_SCENE);
+                    units[i].setWorkLeft(0);
+                    arrivedThisTick =true; // start counting from next tick
+                    // mark incident in progress
+                    for(int k = 0; k < incidentCounter; k++){
+                        if(incidents[k].getIncidentId() == units[i].getIncident()){
+                        incidents[k].setStatus(IncidentStatus.IN_PROGRESS);
+                        break; // incident status needs to be in progress
+                    }
+                }
+            }
+        }
+    }
+
+                // covering the case of already on scene 
+                if(units[i].getUnitStatus() == UnitStatus.AT_SCENE && !arrivedThisTick){
+                    units[i].setWorkLeft(units[i].getWork() + 1);
+                    // ticks below shouldn't ever be less than work but still f
+                if(units[i].getWork() == units[i].getTicksUntilDone()){
+                    units[i].setWorkLeft(-10);// marks that the incident is done 
+                }
+            }
+        }
+        // resolve incidents in ascending incidentId order
+        for(int inc = 0; inc < incidentCounter; inc++){
+            if(incidents[inc] == null) continue;
+                 for(int u = 0; u < unitCounter; u++){
+                    if(units[u] == null) continue;
+                    if(units[u].getIncident() == incidents[inc].getIncidentId()
+                    && units[u].getWork() == -10){
+
+            // resolve incident
+            incidents[inc].setStatus(IncidentStatus.RESOLVED);
+
+            // free unit
+            units[u].setUnitStatus(UnitStatus.IDLE);
+            units[u].setIncident(-10);
+            units[u].setWorkLeft(0);
+
+            break;
+        }
+    }
+    }
     }
 
     @Override
     public String getStatus() {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not implemented yet");
+        // get the tick first then 
+        String status = "TICK=" + tick + "\n";
+        // get obstacle count as don't have thar
+        int obsCount = 0;
+        for(int i = 0; i<cityMap.getHeight(); i++){
+            for (int j=0;j<cityMap.getWidth();j++){
+                if(cityMap.isBlocked(j,i)){
+                    obsCount++;
+                }
+            }
+        }
+        // now can add to the string 
+        status += "STATIONS=" + stationCounter + 
+          " UNITS=" + unitCounter + 
+          " INCIDENTS=" + incidentCounter + 
+          " OBSTACLES=" + obsCount + "\n";
+
+        // now the incidents section 
+        status += "INCIDENTS\n";
+        // need to loop through all 
+        for (int k =0; k< incidentCounter; k++){
+            if(incidents[k] == null){continue;}
+            String unitAss = "-"; // means no unit assigned
+            for (int h=0; h<unitCounter; h++){
+                if(units[h] !=null && units[h].getIncident() == incidents[k].getIncidentId()){
+                    unitAss= "" + units[h].getUnitId();
+                    break;
+                }
+            }
+            status += "I#" + incidents[k].getIncidentId() +
+                  " TYPE=" + incidents[k].getIncidentType() +
+                  " SEV=" + incidents[k].getSeverity() +
+                  " LOC=(" + incidents[k].getX() + "," + incidents[k].getY() + ")" +
+                  " STATUS=" + incidents[k].getIncidentStatus() +
+                  " UNIT=" + unitAss + "\n";
+
+        }
+        // now need to do the units part 
+        status += "UNITS\n";
+            for(int g = 0; g < unitCounter; g++) {
+                if(units[g] == null) continue;
+                String incAss = "-";
+                if(units[g].getIncident() != -10) { // using -10 for no incident
+                    incAss = "" + units[g].getIncident();
+                }
+
+                String workAss = "";
+                    if(units[g].getUnitStatus() == UnitStatus.AT_SCENE) {
+                    workAss = " WORK=" + units[g].getWork();
+                    }
+
+                status += "U#" + units[g].getUnitId() +
+                  " TYPE=" + units[g].getUnitType() +
+                  " HOME=" + units[g].getStationID() +
+                  " LOC=(" + units[g].getX() + "," + units[g].getY() + ")" +
+                  " STATUS=" + units[g].getUnitStatus() +
+                  " INCIDENT=" + incAss + workAss + "\n";
     }
+    return status;
+}
 }
