75fdad93326da1edec3c7e8e3020088adff0e9d8
ac906 <ajec203@exeter.ac.uk>
2026-03-04 13:37:55 +0000

    Clarity on super() to call the parent class constructor

--- cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
+++ cityrescue-starter/src/main/java/cityrescue/PoliceCar.java
@@ -7,6 +7,9 @@ import cityrescue.enums.UnitType;
 public class PoliceCar extends Unit {
    public PoliceCar (int id, int stationId, int x, int y) {
         super(id, stationId, x, y, UnitType.POLICE_CAR, UnitStatus.IDLE);
+        // need super to run the parent constructor in the unit class, 
+        // then set specific values to police car, i found on W3S schools 
+        // when asking how to call parent class constructor from a child class
     }
     public boolean incidentsResolve(IncidentType type) {
     return type == IncidentType.CRIME;
